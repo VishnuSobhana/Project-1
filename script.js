673b7476b0308d5296bd5cb402206719821d9fbf
Vishnu <svishnu987@gmail.com>
2023-02-02 12:32:19 +0100

    added sounds

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const questions = [
+let questions = [
   {
     title:
       "Lewis Hamilton won the first F1 world championship title when he raced for which team?",
@@ -14,28 +14,62 @@ const questions = [
   {
     title:
       "What is the nickname of Ferrari – one of the most successful racing teams in F1 history?",
-    choices: ["The Dancing Donkey", "The Prancing Horse", "The Prancing Pony"],
+    choices: [
+      "The Dancing Donkey",
+      "The Prancing Horse",
+      "The Prancing Pony",
+      "Silver Arrows",
+    ],
     correctAnswer: "The Prancing Horse",
+  },
+  {
+    title: "Which movie was made about Niki Lauda’s racing career?",
+    choices: ["Crash", "Rush", "Champion", "Ford vs Ferrari"],
+    correctAnswer: "Rush",
+  },
+  {
+    title: "Which driver won the first ever F1 world championship title?",
+    choices: [
+      "Juan Manuel Fangio",
+      "Alberto Ascari",
+      "Giuseppe Farina",
+      "Tazio Nuvolari",
+    ],
+    correctAnswer: "Giuseppe Farina",
+  },
+  {
+    title:
+      "Ford provided engines to F1 for many years before forming its own team in 2000 under the name of which subsidiary marques?",
+    choices: ["Mazda", "Volvo", "Jaguar", "Ford"],
+    correctAnswer: "Jaguar",
+  },
+  {
+    title:
+      " Which circuit held the first race of the French Grand Prix in 1950?",
+    choices: ["Reims", "Paul Ricard", " Magny Cours", "Le Mans"],
+    correctAnswer: "Reims",
   },
 ];
 let turn = 0;
 let score = 0;
-//let finalScore = 0;
+
+const audio = document.querySelector("audio");
+
 
 //display question
-// shuffleQuestions();
-// console.log(questions);
-// function shuffleQuestions() {
-//   const shuffeled = [];
+shuffleQuestions();
+function shuffleQuestions() {
+  const shuffeled = [];
 
-//   while (questions.length) {
-//     const randomIndex = Math.floor(Math.random() * questions.length);
+  while (questions.length) {
+    const randomIndex = Math.floor(Math.random() * questions.length);
 
-//     const randomQuestion = questions.splice(randomIndex, 1)[0];
-//     shuffeled.push(randomQuestion);
-//   }
-//   questions = shuffeled;
-// }
+    const randomQuestion = questions.splice(randomIndex, 1)[0];
+    shuffeled.push(randomQuestion);
+  }
+  questions = shuffeled;
+}
+
 function displayQuestion(que) {
   //select questions
   let question = document.querySelector("#each-question");
@@ -47,10 +81,6 @@ function displayQuestion(que) {
     button.innerHTML = questions[turn].choices[index];
   });
 }
-//displayQuestion(questions[turn]);
-
-// turn++
-// displayQuestion(questions[turn]);
 
 // display next Question
 function nextQuestion() {
@@ -72,7 +102,7 @@ function endGame() {
 
   let finalResult = document.createElement("h1");
   //win
-  if (score > 2) {
+  if (score >= 4) {
     finalResult.innerHTML = " you won finalScore " + score;
     let endResult = document.querySelector(".final-score");
     endResult.appendChild(finalResult);
@@ -86,7 +116,7 @@ function endGame() {
   }
 }
 
-// adding eventListeners to the button and checking if it correct
+// adding eventListeners
 const buttons = document.querySelectorAll(".btn").forEach((button) => {
   button.addEventListener("click", function (event) {
     console.log(event.target.innerHTML);
@@ -95,6 +125,14 @@ const buttons = document.querySelectorAll(".btn").forEach((button) => {
       score++;
       document.getElementById("score").innerHTML = score;
     }
+
+    setTimeout(audio.play(), 200);
+    setTimeout(() => {
+      audio.pause();
+      audio.currentTime = 0;
+    }, 3000);
+  
+
     nextQuestion();
   });
 });
